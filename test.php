2c4029c1ccef4c13baf677899e4ace5c6aa8d4ed
Rafael Laurindo <rafaelplaurindo@gmail.com>
2018-06-18 21:33:52 -0300

    fix test

--- test.php
+++ test.php
@@ -3,6 +3,7 @@
 require_once('./autoload.php');
 
 use SM2P\SMTP\OutlookStrategy;
+use SM2P\SMTPStrategyContext;
 
 $options = array(
 
@@ -12,13 +13,13 @@ $strategy = new OutlookStrategy('', $options);
 $smtpStrategyContext = new SMTPStrategyContext($strategy);
 
 $strategy->setPassword('');
-$strategy->addTo('');
 $strategy->addTo('', '');
+$strategy->addTo('');
 $strategy->setSubject('subject test');
 $strategy->setBody('<b>body test</b>');
 
 if ($smtpStrategyContext->send()) {
-    echo 'email sent.';
+    echo "email sent.\n";
 } else {
-    echo 'email doesn\'t sent.';
+    echo "email doesn\'t sent.\n";
 }
