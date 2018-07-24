4d910c1444dd7a408119a638efda944bd12055e5
Rafael Laurindo <rafaelplaurindo@gmail.com>
2018-07-24 18:34:26 -0300

    test migration

--- src/Clients/SMTP/SeSeg.php
+++ src/Clients/SMTP/SeSeg.php
@@ -4,7 +4,6 @@ namespace SM2P\Clients\SMTP;
 
 use SM2P;
 use SM2P\Visitors\Streaming;
-use SM2P\Visitors\Mail;
 use SM2P\Visitors\SMTP;
 
 // Client
@@ -16,32 +15,35 @@ class SeSeg extends SM2P\SMTP {
         parent::__construct('mail.seseg.rj.gov.br', 25, $sender, $options);
 
         $this->commands = new SM2P\Transmissions();
-
         $this->commands->add($this);
     }
 
     function send() {
-//        $sent = false;
+        $sent = false;
 
         $this->commands->accepts2Each(new SMTP\EHLO());
 //        $this->commandInvoker->send('EHLO');
+        $this->commands->accepts2Each(new SMTP\Sender());
 //        $this->commandInvoker->send('SENDER');
+        $this->commands->accepts2Each(new SMTP\Recipients());
 //        $this->commandInvoker->send('RECIPIENTS');
+        $this->commands->accepts2Each(new SMTP\Header());
 //        $this->commandInvoker->send('HEADER');
 //
+        $this->commands->accepts2Each(new SMTP\Body());
 //        $this->commandInvoker->send('BODY');
-//        if ($this->getResponseCode() == '250') {
-//            $sent = true;
-//        }
-//
+        if ($this->getResponseCode() == '250') {
+            $sent = true;
+        }
+
+        $this->commands->accepts2Each(new Streaming\QUIT());
 //        $this->commandInvoker->send('QUIT');
-//
-//        $this->closeConnection();
-//
-//        if ($sent) {
-//            return true;
-//        }
-//        return false;
+
+        $this->closeConnection();
+        if ($sent) {
+            return true;
+        }
+        return false;
     }
 
 }
